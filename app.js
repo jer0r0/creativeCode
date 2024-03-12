0b9838a76e64766e146a77f0170dd95f3943dcd6
JERORO <jeronimo.orozco@urosario.edu.co>
2024-03-12 17:44:26 -0500

    Se hace lo primero de canvas

---
+++ app.js
@@ -0,0 +1,16 @@
+let canvas = document.querySelector('canvas');
+let context = canvas.getContext('2d');
+
+context.fillStyle ="#F9F6EE"
+context.fillRect(100,100,400,400);
+// (w,x,y,z) w=x when start, x=when start, y= xwhen start drawing, z=y when finish drawing
+
+context.lineWidth=4;
+context.beginPath();
+context.rect(100,100,400,400);
+context.stroke();
+
+context.beginPath();
+context.arc(300,300,200, 0,Math.PI*2);
+// 200 in this case is the radious of the arc
+context.stroke();
